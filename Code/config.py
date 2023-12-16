b941f5b4c84405c218b4f12b330c287637a0e311
InesVATI <ines.vati@eleves.enpc.fr>
2023-12-16 01:09:31 +0100

    add expé excursions

---
+++ Code/config.py
@@ -0,0 +1,34 @@
+protocol_1 ={
+    'FILTER' : 'high', # 'high' or 'low'
+    'TYPE_FILTER' : 'non_continuous', # 'continuous' or 'non_continuous' (article's)
+    'PERCENTILE' : [5, 95],
+    'RANDOMIZATION' : 'graph', # 'graph' or 'Fourier'
+}
+
+protocol_2 ={
+    'FILTER' : 'low', # 'high' or 'low'
+    'TYPE_FILTER' : 'non_continuous', # 'continuous' or 'non_continuous' (article's)
+    'PERCENTILE' : [5, 95],
+    'RANDOMIZATION' : 'graph', # 'graph' or 'Fourier'
+}
+
+protocol_3 ={
+    'FILTER' : 'high', # 'high' or 'low'
+    'TYPE_FILTER' : 'continuous', # 'continuous' or 'non_continuous' (article's)
+    'PERCENTILE' : [5, 95],
+    'RANDOMIZATION' : 'graph', # 'graph' or 'Fourier'
+}
+
+protocol_4 ={
+    'FILTER' : 'low', # 'high' or 'low'
+    'TYPE_FILTER' : 'continuous', # 'continuous' or 'non_continuous' (article's)
+    'PERCENTILE' : [5, 95],
+    'RANDOMIZATION' : 'graph', # 'graph' or 'Fourier'
+}
+
+protocol_5 ={
+    'FILTER' : 'low', # 'high' or 'low'
+    'TYPE_FILTER' : 'continuous', # 'continuous' or 'non_continuous' (article's)
+    'PERCENTILE' : [5, 95],
+    'RANDOMIZATION' : 'Fourier', # 'graph' or 'Fourier'
+}
